d1c83973e0a43ad9b17b7c983a2dc134d278dd72
dwanderson <anderson.dan.w@gmail.com>
2016-05-11 20:30:33 -0400

    Pointer methods can modify their pointee

--- methods.go
+++ methods.go
@@ -19,10 +19,24 @@ func (s SpaceCoord) Distance() float64 {
 	return math.Sqrt(x)
 }
 
+// note, *value* receivers (as above) cannot change their value, but
+// but *pointer* receivers can change their pointer
+// to whit - if you remove the '*', s stays the same outside the func
+func (s *SpaceCoord) Scale(scale float64) {
+	s.x *= scale
+	s.y *= scale
+	s.z *= scale
+	fmt.Println("Inside Scale - size:", s.Distance())
+}
+
 func main() {
 	fmt.Println("Methods")
 	flatZ := SpaceCoord{x: 3, y: 4}
 	fmt.Println(flatZ.Distance())
 	s := SpaceCoord{6, 6, 7}
+	fmt.Println(s.Distance())
+
+	fmt.Println("Scale that last point by 4")
+	s.Scale(4)
 	fmt.Println(s.Distance())
 }
